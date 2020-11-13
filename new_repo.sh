8d032fca8ba2f28d04e47324128de1c550a90a9a
guillem <guillempejo@gmail.com>
2020-11-13 18:56:24 +0100

    Fixed the old API call username/password for basic auth and changed to access token because the first is now deprecated

--- new_repo.sh
+++ new_repo.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 USERNAME=''
 PASSWORD=''
+MY_ACCESS_TOKEN=''
 echo "Hi! What do you want to call your new repository?"
 read REPO_NAME
 echo "Nice! New repo $REPO_NAME created! And now what about the description?"
 read DESCRIPTION
-curl -u ${USERNAME}:${PASSWORD} https://api.github.com/user/repos -d "{\"name\": \"${REPO_NAME}\", \"private\": false, \"description\": \"${DESCRIPTION}\"}"
+# DEPRECATED SINCE November 13, 2020 at 16:00 UTC 
+# curl -u ${USERNAME}:${PASSWORD} https://api.github.com/user/repos -d "{\"name\": \"${REPO_NAME}\", \"private\": false, \"description\": \"${DESCRIPTION}\"}"
+
+curl -H "Authorization: token $MY_ACCESS_TOKEN" https://api.github.com/user/repos -d "{\"name\": \"${REPO_NAME}\", \"private\": false, \"description\": \"${DESCRIPTION}\"}"
 echo "Done!. Go to https://github.com/$USERNAME/$REPO_NAME to see." 
 
 # git init
